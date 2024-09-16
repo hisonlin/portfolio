e4f2ee45de035819913ecde35bb3705ce6bb3cc3
hisonlin <hisonlinca@gmail.com>
2024-09-16 15:46:02 -0700

    update resume url

--- src/Component/HomeMain/HomeMain.jsx
+++ src/Component/HomeMain/HomeMain.jsx
@@ -5,7 +5,7 @@ import Logo from '../Logo/Logo';
 
 const urls = {
     linkedin: 'https://www.linkedin.com/in/hisonlin/',
-    resume: 'https://drive.google.com/file/d/1jLZNp4XG3B4y68rMA5bK6gee9YbesJNP/view?usp=drive_link',
+    resume: 'https://drive.google.com/file/d/1V5XUr3hSs6tZlaTF3L_Srq9Pj_cyBqJy/view?usp=drive_link',
     github: 'https://github.com/hisonlin'
   };
   
